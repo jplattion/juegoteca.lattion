768014611ac0034639e90800a5acd340aeb9b966
Juan Pablo Lattion <jplattion@gmail.com>
2022-06-20 02:04:02 -0300

    first commit

---
+++ src/components/Navbar/NavBar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./NavBar.css";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import logo from "./assets/logo.png";
+
+const NavBar = () => {
+	return (
+		<>
+			<Navbar expand="md" className="containerProject">
+				<Container fluid>
+					<Navbar.Brand href="#" className="title">
+						<img src={logo} alt="logo" className="logo" />
+						Dados y Meeples
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="navbarScroll" />
+					<Navbar.Collapse id="navbarScroll">
+						<Nav className="ms-auto my-2 my-md-0" navbarScroll>
+							<Nav.Link href="#inicio!">Inicio</Nav.Link>
+							<Nav.Link href="#juegos!">Juegos</Nav.Link>
+							<Nav.Link href="#contacto!">Contacto</Nav.Link>
+						</Nav>
+						<ShoppingCartIcon className="shoppingCart" />
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
+		</>
+	);
+};
+
+export default NavBar;
